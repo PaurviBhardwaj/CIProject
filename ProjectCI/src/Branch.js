3d75fcb43a11e4d580805cecaae5ea7cb631c2a9
srishtinanda <nanda.srishti5494@gmail.com>
2017-01-02 18:45:12 +0530

    testsettings commit

--- ProjectCI/src/Branch.js
+++ ProjectCI/src/Branch.js
@@ -1,41 +1,34 @@
-import React,{Component} from 'react';
-import Paper from 'material-ui/Paper';
-import {List, ListItem} from 'material-ui/List';
-import Checkbox from 'material-ui/Checkbox';
-import RaisedButton from 'material-ui/RaisedButton';
-import {IndexLink, Link} from 'react-router';
+	import React, { Component } from 'react';
+	import Paper from 'material-ui/Paper';
+	import { List, ListItem } from 'material-ui/List';
+	import Checkbox from 'material-ui/Checkbox';
+	import RaisedButton from 'material-ui/RaisedButton';
+	import { IndexLink, Link } from 'react-router';
+	import TextField from 'material-ui/TextField';
+	import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import Toggle from 'material-ui/Toggle';
 
-const styles={
-	heading:{
-		textColor:'black',
-		fontSize:'30',
-		marginLeft:'300',
-		marginTop:'100',
-	},
-	list:{
-		margin:'auto',
-		width:400,
-	},
-	bar:{
-		marginLeft:150,
-	}
-};
-class Branch extends Component{
-	render(){
-		return(
-			<div>
-			<p style={styles.heading}>Adding branches to your current repository necessary to run CI </p>
-			<Paper style={styles.list}><List>
-			<ListItem primaryText="master" leftCheckbox={<Checkbox checked={true} disabled={true}/>} />
-		    <ListItem primaryText="dev" leftCheckbox={<Checkbox checked={true} disabled={true}/>}/>
-		    <ListItem primaryText="testing" leftCheckbox={<Checkbox checked={true} disabled={true}/>} />
-			</List> 
-			<IndexLink to="/App/branchpath" activeClassName="active"><RaisedButton label="Ready for CI" primary={true} style={styles.bar}></RaisedButton></IndexLink>
-			<br/>
-			</Paper>
-			</div>
-			);
-	}
-}
+	class Branch extends Component{
+		render(){
+			return(
+				<Paper>
+					<TextField
+      			hintText="No.of teams"
+      			floatingLabelText="Enter the number of teams working in this project."
+    			/><br />
+					<h4>Adding branches to your current repository necessary to run CI </h4>
+					
+						<List>
+							<ListItem primaryText="master(protected)" leftCheckbox={<Checkbox checked={true} disabled={true}/>} />
+				  		<ListItem primaryText="dev" leftCheckbox={<Checkbox checked={true} disabled={true}/>}/>
+				  		<ListItem primaryText="testing" leftCheckbox={<Checkbox checked={true} disabled={true}/>} />
+						</List> 
+						<IndexLink to="/App/branchpath" activeClassName="active"><RaisedButton label="Ready for CI" primary={true}></RaisedButton></IndexLink>
+						<br/>
+					</Paper>
+				
+				);
+			}
+		}
 
 export default Branch;
--- ProjectCI/src/testSettings.js
+++ ProjectCI/src/testSettings.js
@@ -3,61 +3,40 @@ import Paper from 'material-ui/Paper';
 import {List, ListItem} from 'material-ui/List';
 import Checkbox from 'material-ui/Checkbox';
 import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {white} from 'material-ui/styles/colors';
 import TextField from 'material-ui/TextField';
 import {IndexLink, Link} from 'react-router';
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import './App.css';
+import {Grid, Row, Col} from 'react-flexbox-grid';
 
-const styles= {
-	style:{
-	  width: 960,
-	  margin: 20,
-	  textAlign: 'center',
-	  margin: 'auto',
+const styles={
+  next:{
+    marginBottom:80,
   },
-  heading:{
-  	paddingTop:'80',
-  	textAlign:'center',
-  	fontSize :'46px',
-  	margin:'auto',
-  },
-  setupList: {
-  	width:'960',
-  	marginLeft:'272',
-  	backgroundColor:'black',
-  	textColor:'white',
-    },
-  list: {
-  	textAlign:'left',
-  },
-  click:{
-  	textAlign:'center',
-  	marginLeft:'100',
-  },
-  checklist:{
-  	textAlign:'center',
-  	color:'black',
-  	fontSize:'30px',
-  },
-  bar:{
-  	marginBottom:'80',
-  	marginLeft:'630',
-  },
-  box:{
-  	paddingLeft:'280',
-  	fontSize:'30',
-  	margin:'0',
-  },
-  add:{
-  	marginLeft:'750',
+  top:{
+    marginTop:80
   }
-
 };
+const muiTheme = getMuiTheme({
+ palette: {
+   textColor: white,
+ }
+});
 
 class TestSettings extends Component{
-	render(){
-		var text = `# By default we use the Node.js version set in your package.json or the latest
+  
+  render(){
+    const CheckboxIcon=[
+      <Checkbox></Checkbox>
+    ];
+    var text = `# By default we use the Node.js version set in your package.json or the latest
 # version from the 0.10 release
 # You can use nvm to install any Node.js (or io.js) version you require.
 # nvm install 4.0
@@ -65,44 +44,166 @@ nvm install 0.10
 npm install
 # Install grunt-cli for running your tests or other tasks
 # npm install -g grunt-cli`
- 
-		return(
-
-			<div>
-			<p style={styles.heading}>Test Settings</p><br/>
-				<div>
-					<p style={styles.box}>Setup Commands</p>
-			<TextField style={styles.setupList}
-			multiLine={true}
-			defaultValue={text} /><br/>
-			</div>
-      <Paper style={styles.style} zDepth={2} rounded={true}>
-      <p style={styles.checklist}>
-			Pipeline
-			</p>
-			<FlatButton label="+ Add to Pipeline" primary={true} style={styles.add}/>
-			<List style={styles.list}>
-				<ListItem primaryText="Linting" leftCheckbox={<Checkbox />}/>
-		        <ListItem primaryText="Test Cases" leftCheckbox={<Checkbox />} />
-		        <ListItem primaryText="Test Coverage" leftCheckbox={<Checkbox />} />
-		        <ListItem primaryText="Tagging" leftCheckbox={<Checkbox />} />
-		        <ListItem primaryText="Update Commit Status" leftCheckbox={<Checkbox />} />
-		        <ListItem primaryText="Test Cases" leftCheckbox={<Checkbox />} />
-      </List>
-	<Card >
-    <CardHeader style={styles.click}
-      title="TEST"
-      actAsExpander={true}/>
-     <CardText expandable={true} >
-      Successful
-    </CardText>
-      </Card>
-			</Paper>
-			<br/>
-			<IndexLink to="/App/temp" activeClassName="active"><RaisedButton label="Save and go to dashboard" primary={true} style={styles.bar}></RaisedButton></IndexLink>
-			</div>
-			);
-	}
-
+    return(
+      <Paper>
+        <p style={styles.top}> 
+        <Grid>
+          <Row>
+            <Col xs={12}>
+            <Row center="xs">
+              <Col xs={12}><h1>Test Settings</h1><br/></Col>
+            </Row>
+            </Col>
+          </Row>
+        </Grid>
+        </p>
+        <h2>Setup Commands</h2>
+        <Paper style={{backgroundColor:'black'}}>
+        <MuiThemeProvider muiTheme={muiTheme}>
+         <Grid>
+          <Row>
+            <Col xs={12}>
+              <Row start="xs">
+                <Col xs={12}>
+                  <TextField 
+                    multiLine={true}
+                    defaultValue={text}
+                    fullWidth={true} 
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
+        </MuiThemeProvider>
+        </Paper>
+        <Paper zDepth={2} rounded={true}>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <Row center="xs">
+                <Col xs={12}><h2>Pipeline</h2></Col>
+              </Row>
+            </Col>
+          </Row>
+        </Grid>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <Row start="xs">
+                <Col xsOffset={1} sm={3} xs={8} >
+                  <TextField 
+                    hintText= "Add to Pipeline" 
+                  /></Col><Col  sm={8} xs={3}><FloatingActionButton mini={true} primary={true}>
+                  <ContentAdd />
+                  </FloatingActionButton>
+                </Col>
+              </Row>
+              <Row start="xs">
+                <Col xs={12}>
+                <List>
+                 <ListItem>
+                  <Card>
+                    <CardHeader
+                      subtitle="Select linting tool"
+                      actAsExpander={true}
+                      showExpandableButton={false}
+                    > 
+                    <Toggle label="Linting"></Toggle> </CardHeader>
+                    <CardText expandable={true}>
+                      <List>
+                        <ListItem primaryText="EsLint" leftCheckbox={<Checkbox/>}/>
+                        <ListItem primaryText="HTML Lint" leftCheckbox={<Checkbox/>}/>
+                      </List>
+                    </CardText>
+                  </Card>
+                </ListItem>
+                <ListItem>
+                  <Card>
+                    <CardHeader
+                      subtitle="Suggestion :Keep your files to be tested in a 'TEST' directory."
+                      actAsExpander={true}
+                      showExpandableButton={false}
+                    > 
+                    <Toggle label="Test Cases(mocha)"></Toggle> </CardHeader>
+                    <CardText expandable={true}>
+                      <TextField
+                        multiLine={true}
+                        floatingLabelText="Mention your file name or regex."
+                      /><br/>
+                    </CardText>
+                  </Card>
+                </ListItem>
+                <ListItem>
+                  <Card>
+                    <CardHeader
+                      title=""
+                      actAsExpander={false}
+                    >
+                    <Toggle label="Test Coverage"></Toggle>
+                    </CardHeader>
+                  </Card>
+                </ListItem>
+                <ListItem>
+                  <Card>
+                    <CardHeader 
+                      title=""
+                      actAsExpander={false}
+                    >
+                    <Toggle label="Tagging"></Toggle>
+                    </CardHeader>
+                  </Card>
+                </ListItem>
+                <ListItem>
+                  <Card>
+                    <CardHeader
+                      title=""
+                      actAsExpander={false}
+                    >
+                    <Toggle label="Update Commit Status"></Toggle>
+                    </CardHeader>
+                  </Card>
+                </ListItem>
+              </List>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid> 
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <Row center="xs">
+              <Col xs={12}>
+                <Card>
+                  <CardHeader
+                    actAsExpander={true}
+                    title="TEST"
+                  />
+                  <CardText expandable={true} >
+                    Successful
+                  </CardText>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+      <br/>
+    </Paper>
+    <br/>
+    <Grid>
+      <Row>
+        <Col xs={12}>
+          <Row center="xs">
+            <Col xs={12}><IndexLink to="/App/temp" activeClassName="active"><RaisedButton label="Save and go to dashboard" primary={true} style={styles.next}></RaisedButton></IndexLink>
+            </Col>
+          </Row>
+       </Col>
+      </Row>
+    </Grid>
+    </Paper>
+    );
+  }
 }
 export default TestSettings;
