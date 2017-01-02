a8851004f5c809f8757574e87667ea1dfd889772
Julia Paul <juliapaulmyla@yahoo.com>
2017-01-02 18:32:37 +0530

    build report second commit

--- ProjectCI/src/App.js
+++ ProjectCI/src/App.js
@@ -7,28 +7,16 @@ class App extends Component {
    return (
      <div>
                <Header />
-             <div>
-<<<<<<< HEAD
-               
+             
 
                 <div className="main" >
                     {this.props.children}
                 </div>
-
+                <Footer/>
                
-            </div>
+    </div>
     );
   }
-=======
-              
-                   {this.props.children}
-             
-               </div>
-               <Footer />
-         </div>      
-   );
- }
->>>>>>> 78de479bb453a62aa04760008fd6e95754ff7f9b
 }
 
 export default App;
